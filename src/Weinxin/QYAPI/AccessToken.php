1221bae790d9744fcb08b2d3d67febffffa788a9
web3d <web3d@live.com>
2015-10-26 12:26:49 +0800

    [CHG] 获取accesstoken独立出来,便于各应用场景灵活获取与存储

--- src/Weinxin/QYAPI/AccessToken.php
+++ src/Weinxin/QYAPI/AccessToken.php
@@ -4,9 +4,24 @@
  * AccessToken是企业号的全局唯一票据，调用接口时需携带AccessToken。
  * 
  */
-class Weixin_QYAPI_AccessToekn extends Weixin_QYAPI_Base {
+class Weixin_QYAPI_AccessToekn {
     
     const API_GET_TOKEN = '/gettoken';
+    
+    protected $corpId = '';
+    protected $corpSecret = '';
+    
+    protected $errorCode;
+    protected $errorMsg;
+    
+    /**
+     * 构造函数、配置企业号相关参数
+     * @param string $corpId 企业号corp_id
+     * @param string $corpSecret 企业号corp_secrect
+     */
+    public function __construct($corpId, $corpSecret) {
+        $this->init($corpId, $corpSecret);
+    }
     
     /**
      * 配置企业号相关参数
@@ -15,29 +30,83 @@ class Weixin_QYAPI_AccessToekn extends Weixin_QYAPI_Base {
      * @param string $corpSecret  企业号corp_secrect
      */
     public function init($corpId, $corpSecret) {
-        $this->corpId = $corpId;
-        $this->corpSecret = $corpSecret;
+        $this->setCorpId($corpId);
+        $this->setCorpSecret($corpSecret);
+    }
+    
+    public function setCorpId($corpId) {
+        if ($corpId) {
+            $this->corpId = $corpId;
+        }
+    }
+
+    public function setCorpSecret($corpSecret) {
+        if ($corpSecret) {
+            $this->corpSecret = $corpSecret;
+        }
+    }
+    
+    public function getCorpId() {
+        return $this->corpId;
+    }
+
+    public function getCorpSecret() {
+        return $this->corpSecret;
+    }
+
+    /**
+     * 设置错误信息
+     *
+     * @param string $code 错误代码
+     * @param string $msg 错误详细信息
+     */
+    public function setError($code, $msg = null) {
+        $this->errorCode = $code;
+        $this->errorMsg = $msg;
+    }
+
+    /**
+     * 获取错误代码
+     *
+     * @return string 错误代码
+     */
+    public function getErrorCode() {
+        return $this->errorCode;
+    }
+
+    /**
+     * 获取错误信息
+     *
+     * @return string 错误信息
+     */
+    public function getErrorMsg() {
+        return $this->errorMsg;
     }
     
     /**
-     * AccessToken需要用CorpID和Secret来换取，不同的Secret会返回不同的AccessToken。正常情况下AccessToken有效期为7200秒，有效期内重复获取返回相同结果；有效期内有接口交互（包括获取AccessToken的接口），会自动续期
-     * @return type
+     * AccessToken需要用CorpID和Secret来换取，不同的Secret会返回不同的AccessToken。
+     * 正常情况下AccessToken有效期为7200秒，有效期内重复获取返回相同结果；
+     * 有效期内有接口交互（包括获取AccessToken的接口），会自动续期
+     * @return string|boolean
      */
-    public function get() {
-        $queryStr = array(
+    public function get($corpId = '', $corpSecret = '') {
+        $this->init($corpId, $corpSecret);
+
+        $res = Weixin_HTTPClient::get(self::API_GET_TOKEN, array(
             'corpid' => $this->corpId,
             'corpsecret' => $this->corpSecrect,
-        );
-
-        $res = self::_get(self::API_GET_TOKEN, '', $queryStr);
+        ));
+        
         if (false === $res) {
             $this->setError(-10, '获取AccessToken失败!');
             return false;
         }
 
-        $token = $res['access_token'];
+        if(empty($res['access_token'])) {
+            $this->setError(-11, '无效AccessToken!');
+            return false;
+        }
 
-
-        return $token;
+        return $res['access_token'];
     }
 }
