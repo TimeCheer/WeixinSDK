0ee54ad9c4b18ffc0881f6d29d24174af2a54a5d
web3d <web3d@live.com>
2015-11-16 19:37:43 +0800

    [CHG] 开始着手公众号部分的接口开发

---
+++ src/Weixin/MP/Base.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * MP平台的访问基类
+ */
+class Weixin_MP_Base extends Weixin_Base {
+    /**
+     * @var string MP API URL PREFIX
+     */
+    protected $apiPrefix = 'https://api.weixin.qq.com/cgi-bin';
+}
