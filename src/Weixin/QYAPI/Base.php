ff9af6f3decd7eb8740a83f8653d9ad48043a73f
web3d <web3d@live.com>
2015-11-16 19:36:54 +0800

    [CHG] 重构Base类

--- src/Weixin/QYAPI/Base.php
+++ src/Weixin/QYAPI/Base.php
@@ -2,104 +2,14 @@
 
 /**
  * API基类
+ * @package weixin.mp
  */
-class Weixin_QYAPI_Base {
-    
-    const API_PREFIX = 'https://qyapi.weixin.qq.com/cgi-bin';
-
-    protected $accessToken;
-    
-    protected $errorCode = null;
-    protected $errorMsg = '';
-
-    /**
-     * 构造函数、配置企业号相关参数
-     * @param string $corpId 企业号corp_id
-     * @param string $corpSecret 企业号corp_secrect
-     */
-    public function __construct($accessToken = '') {
-        if ($accessToken) {
-            $this->init($accessToken);
-        }
-    }
-
-    /**
-     * 配置企业号相关参数
-     *
-     * @param string $corpId      企业号corp_id
-     * @param string $corpSecret  企业号corp_secrect
-     */
-    public function init($accessToken) {
-        $this->accessToken = $accessToken;
-    }
-
-    /**
-     * 设置错误信息
-     *
-     * @param string $code 错误代码
-     * @param string $msg 错误详细信息
-     */
-    public function setError($code, $msg = '') {
-        $this->errorCode = $code;
-        $this->errorMsg = $msg;
-    }
-
-    /**
-     * 获取错误代码
-     *
-     * @return string 错误代码
-     */
-    public function getErrorCode() {
-        return $this->errorCode;
-    }
-
-    /**
-     * 获取错误信息
-     *
-     * @return string 错误信息
-     */
-    public function getErrorMsg() {
-        return $this->errorMsg;
-    }
-
-    /**
-     * 接口请求方法，具体接口都调用此方法进行请求
-     * @param strin $api
-     * @param array $params
-     */
-    public function doGet($api, array $params = array()) {
-        $res = Weixin_HTTPClient::get($api, array_merge(
-            $params,
-            array('access_token' => $this->accessToken)
-        ));
-        
-        if (false === $res) {
-            $this->setError(-10, '获取数据失败!');
-            return false;
-        }
-
-        return $res;
-    }
+class Weixin_QYAPI_Base extends Weixin_Base {
     
     /**
-     * 接口请求方法，具体接口都调用此方法进行请求
-     * @param strin $api
-     * @param array $data post的数据
-     * @param array $params url中构造的参数
+     *
+     * @var string 企业号的url前缀 
      */
-    public function doPost($api, array $data = array(), array $params = array()) {
-        $url = self::API_PREFIX . $api . '?' . http_build_query(array_merge(
-            $params,
-            array('access_token' => $this->accessToken)
-        ));
-        $res = Weixin_HTTPClient::post($url, $data);
-        
-        if (false === $res) {
-            $this->setError(-10, '获取数据失败!');
-            return false;
-        }
-
-        return $res;
-    }
+    protected $apiPrefix = 'https://qyapi.weixin.qq.com/cgi-bin';
 
 }
