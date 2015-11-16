7775d3eeab2a7ca07db6efdade1c8384d4e1962b
web3d <web3d@live.com>
2015-11-16 22:57:38 +0800

    [CHG] 关于OAuth2的代码结构

---
+++ src/Weixin/API/OAuth2.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * OAuth2相关接口
+ * @package timecheer.weixin.api
+ */
+class Weixin_API_OAuth2 extends Weixin_API_Base {
+    
+    const API_TOKEN = '/sns/oauth2/access_token';
+    
+    const API_REFRESH_TOKEN = '/sns/oauth2/refresh_token';
+    
+    const API_USER = '/sns/userinfo';
+    
+    /**
+     * 通过接口换取access token
+     * @param string $appId
+     * @param string $appSecret
+     * @param string $code
+     * @return array 
+     */
+    public function accessToken($appId, $appSecret, $code) {
+        return $this->doGet(self::API_TOKEN, array(
+            'appid' => $appId,
+            'secret' => $appSecret,
+            'grant_type' => 'authorization_code',
+            'code' => $code
+        ));
+    }
+    
+    /**
+     * 刷新access_token
+     * @param string $appId
+     * @param string $refreshToken
+     * @return array
+     */
+    public function refreshAccessToken($appId, $refreshToken) {
+        return $this->doGet(self::API_REFRESH_TOKEN, array(
+            'appid' => $appId,
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $refreshToken
+        ));
+    }
+    
+    /**
+     * 拉取用户信息(需scope为 snsapi_userinfo)
+     * @param string $accessToken
+     * @param string $openId
+     * @return array
+     */
+    public function getUser($accessToken, $openId) {
+        return $this->doGet(self::API_USER, array(
+            'access_token' => $accessToken,
+            'openid' => $openId,
+            'lang' => 'zh_CN'
+        ));
+    }
+}
---
+++ src/Weixin/Open/Base.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Open平台接口
+ * @package timecheer.weixin.open
+ */
+class Weixin_Open_Base extends Weixin_Base {
+    /**
+     *
+     * @var string 开放平台地址前缀 
+     */
+    protected $apiPrefix = 'https://open.weixin.qq.com';
+}
