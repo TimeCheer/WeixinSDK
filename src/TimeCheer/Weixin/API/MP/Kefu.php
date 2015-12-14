3d98539c9b5a21cfa91c6a41ca77d342c7515764
web3d <web3d@live.com>
2015-12-14 19:16:21 +0800

    移植了一部分功能

--- src/TimeCheer/Weixin/API/MP/Kefu.php
+++ src/TimeCheer/Weixin/API/MP/Kefu.php
@@ -8,4 +8,132 @@ namespace TimeCheer\Weixin\API\MP;
  */
 class Kefu extends Base {
     
+    const API_ADD = '/kfaccount/add';
+    const API_UPDATE = '/kfaccount/update';
+    const API_DELETE = '/kfaccount/del';
+    const API_LIST = '/getkflist';
+    const API_LIST_ONLINE = '/getonlinekflist';
+    
+    public $_err_msg = array(
+        '0' => '成功', //(no error)
+        '61451' => '参数错误', //(invalid parameter)
+        '61452' => '无效客服账号', //(invalid kf_account)
+        '61453' => '账号已存在', //(kf_account exsited)
+        '61454' => '账号名长度超过限制(前缀10个英文字符)', //(invalid kf_acount length)
+        '61455' => '账号名包含非法字符(英文+数字)', //(illegal character in kf_account)
+        '61456' => '账号个数超过限制(100个客服账号)', //(kf_account count exceeded)
+        '61457' => '无效头像文件类型', //(invalid file type)
+    );
+
+    /**
+     * 添加客户帐号
+     * 
+     * @param string $kf_acount 完整客服账号，格式为：账号前缀@公众号微信号
+     * @param string $nickname 客服昵称，最长6个汉字或12个英文字符
+     * @param string $password 客服账号登录密码，格式为密码明文的32位加密MD5值
+     * @return boolean
+     */
+    public function add($kf_acount, $nickname, $password) {
+        $params = array(
+            'kf_account' => $kf_acount,
+            'nickname' => $nickname,
+            'password' => $password,
+        );
+        $res = $this->doPost(self::API_ADD, $params);
+
+        if (isset($res['errcode']) && $res['errcode'] === 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 设置客户信息
+     * 
+     * @param string $kf_acount 完整客服账号，格式为：账号前缀@公众号微信号
+     * @param string $nickname 客服昵称，最长6个汉字或12个英文字符
+     * @param string $password 客服账号登录密码，格式为密码明文的32位加密MD5值
+     * @return boolean
+     */
+    public function update($kf_acount, $nickname, $password) {
+        $params = array(
+            'kf_account' => $kf_acount,
+            'nickname' => $nickname,
+            'password' => $password,
+        );
+        $res = $this->doPost(self::API_UPDATE, $params);
+
+        if (isset($res['errcode']) && $res['errcode'] === 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 给客服帐号上传头像
+     * 
+     * @param string $media 头像图片
+     * @param string $kf_account 完整客服账号，格式为：账号前缀@公众号微信号
+     * @return boolean
+     */
+    public function uploadHeadImg($media, $kf_account) {
+        $params = array(
+            'media' => '@' . $media,
+        );
+        $res = $this->doPost(self::API_UPDATE, $params, array('kf_account' => $kf_account));
+
+        if (isset($res['errcode']) && $res['errcode'] === 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 删除客户帐号
+     */
+    public function delete($kf_account) {
+
+        $res = $this->doGet(self::API_DELETE, array('kf_account' => $kf_account));
+
+        if (isset($res['errcode']) && $res['errcode'] === 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 获取客服基本信息
+     * 
+     * @return boolean|array array(array('kf_account'=>'***', 'kf_headimgurl'=>'***', 'kf_id'=>'1001', 'kf_nick'=>'***'),...)
+     */
+    public function getKFList() {
+
+        $res = $this->doGet(self::API_LIST);
+
+        if (!isset($res['kf_list'])) {
+            return false;
+        }
+
+        return $res['kf_list'];
+    }
+
+    /**
+     * 获取在线客户接待状态
+     * 
+     * @return boolean|array array(array('kf_account'=>'***', 'status'=>1, 'kf_id'=>'1001', 'auto_accept'=>0, 'accepted_case'=>1),...)
+     */
+    public function getOnlineKFList() {
+
+        $res = $this->doGet(self::API_LIST_ONLINE);
+
+        if (!isset($res['kf_online_list'])) {
+            return false;
+        }
+
+        return $res['kf_online_list'];
+    }
 }
--- src/TimeCheer/Weixin/API/MP/Mass.php
+++ src/TimeCheer/Weixin/API/MP/Mass.php
@@ -8,4 +8,148 @@ namespace TimeCheer\Weixin\API\MP;
  */
 class Mass extends Base {
     
+    const API_GET = '/mass/get';
+    const API_SEND = '/mass/send';
+    const API_SEND_ALL = '/mass/getall';
+    const API_DELETE = '/mass/delete';
+
+    /**
+     * 查询群发消息发送状态
+     * 
+     * @param int $msg_id
+     * @return string|boolean SEND_SUCCESS表示发送成功
+     */
+    public function get($msg_id) {
+        $params = array(
+            'msg_id' => $msg_id,
+        );
+        $res = $this->doPost(self::API_GET, $params);
+
+        return !empty($res['msg_status']) ? $res['msg_status'] : false;
+    }
+
+    /**
+     * 发送消息
+     * 
+     * @param string|array $data
+     * @param boolean $is_all send/sendall
+     * @return boolean
+     */
+    public function send($data, $is_all = false) {
+        if (is_array($data)) {
+            $data = json_encode($data);
+        } else if (!json_decode($data)) {
+            return false;
+        }
+        $res = $this->httpRequest($is_all ? self::API_SEND_ALL : self::API_SEND, $data);
+
+        if (isset($res['errcode']) && $res['errcode'] == 0) {
+            return $res;
+        }
+
+        return false;
+    }
+
+    /**
+     * 删除群发
+     * <pre>
+     * 1、只有已经发送成功的消息才能删除
+     * 2、删除消息是将消息的图文详情页失效，已经收到的用户，还是能在其本地看到消息卡片。
+     * 3、删除群发消息只能删除图文消息和视频消息，其他类型的消息一经发送，无法删除。
+     * 4、如果多次群发发送的是一个图文消息，那么删除其中一次群发，就会删除掉这个图文消息也，导致所有群发都失效
+     * </pre>
+     * @param int $msg_id 发送出去的消息ID
+     * @return boolean
+     */
+    public function delete($msg_id) {
+        $params = array(
+            'msg_id' => $msg_id,
+        );
+        $res = $this->httpRequest($this->url . 'mass/delete?' . http_build_query($this->getParams()), $params, 'post');
+
+        if (isset($res['errcode']) && $res['errcode'] == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 根据分组进行群发【订阅号与服务号认证后均可用】
+     * 
+     * @param string $media_id
+     * @param string $msgtype 'mpnews', 'text', 'image', 'voice', 'mpvideo','wxcard'
+     * @param boolean $is_to_all
+     * @param int $group_id
+     * @return false|array
+     */
+    public function sendByGroup($media_id, $msgtype = 'mpnews', $is_to_all = TRUE, $group_id = 0) {
+        $params = array(
+            'filter' => array('is_to_all' => ($is_to_all ? true : false)),
+            $msgtype => array('media_id' => $media_id),
+            'msgtype' => $msgtype
+        );
+        if (!$is_to_all) {
+            if (!is_numeric($group_id) || $group_id < 0) {
+                return false;
+            }
+            $params['filter']['group_id'] = $group_id;
+        }
+
+        $res = $this->httpRequest($this->url . 'mass/sendall?' . http_build_query($this->getParams()), $params, 'post');
+
+        if (isset($res['errcode']) && $res['errcode'] == 0) {
+            return $res;
+        }
+
+        return false;
+    }
+
+    /**
+     * 根据OpenID列表群发【订阅号不可用，服务号认证后可用】
+     * 
+     * @param string|array $media
+     * @param string $msgtype 'news', 'mpnews', 'text', 'image', 'voice', 'video','wxcard'
+     * @param array|string $openids
+     * @return false|array
+     */
+    public function sendByOpenid($media, $msgtype = 'mpnews', $openids = null) {
+        if (empty($openids)) {
+            return false;
+        }
+        if (!is_array($openids)) {
+            $openids = explode(',', $openids);
+        } else {
+            $openids = array_values($openids);
+        }
+
+        $params = array(
+            'touser' => $openids,
+            'msgtype' => $msgtype,
+            $msgtype => array('media_id' => $media)
+        );
+        switch ($msgtype) {
+            case 'wxcard':
+                $params[$msgtype] = array('card_id' => $media);
+                break;
+            case 'text':
+                $params[$msgtype] = array('content' => $media);
+                break;
+            case 'video':
+                $params[$msgtype]['title'] = $media['title'];
+                $params[$msgtype]['description'] = $media['description'];
+                break;
+            default:
+                break;
+        }
+
+        $res = $this->httpRequest($this->url . 'mass/send?' . http_build_query($this->getParams()), $params, 'post');
+
+        if (isset($res['errcode']) && $res['errcode'] == 0) {
+            return $res;
+        }
+
+        return false;
+    }
+
 }
